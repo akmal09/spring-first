87d0c2efa620c049faeaea80d10bb7e6a539a4bf
akmal09 <akmal.119140203@student.itera.ac.id>
2023-09-01 09:40:43 +0700

    pahamin konsep nerima request body

--- src/main/java/com/example/departmentexample/controller/DepartmentController.java
+++ src/main/java/com/example/departmentexample/controller/DepartmentController.java
@@ -1,15 +1,21 @@
 package com.example.departmentexample.controller;
 
 import com.example.departmentexample.entity.Department;
+import com.example.departmentexample.helper.RequestJson;
 import com.example.departmentexample.service.DepartmentService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @RestController
 public class DepartmentController {
     @Autowired private DepartmentService departmentService;
+    private static final Logger logger = LoggerFactory.getLogger(DepartmentController.class);
 
     @PostMapping("/departments")
     public Department saveDepartment(
@@ -40,4 +46,21 @@ public class DepartmentController {
         departmentService.deleteDepartmentById(departmentId);
         return "Deleted Success";
     }
+
+    @GetMapping("/tes")
+    public static void tes(
+            @RequestBody String tes
+    ){
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            RequestJson requestJson = objectMapper.readValue(tes,RequestJson.class);
+            System.out.println("Name: " + requestJson.getName());
+            System.out.println("Age: " + requestJson.getAge());
+            System.out.println("City: " + requestJson.getCity());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        logger.info("helooowjiaodsof "+ tes);
+    }
+
 }
---
+++ src/main/java/com/example/departmentexample/helper/RequestJson.java
@@ -0,0 +1,19 @@
+package com.example.departmentexample.helper;
+
+public class RequestJson {
+    private String name;
+    private int age;
+    private String city;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public Integer getAge(){
+        return this.age;
+    }
+
+    public String getCity(){
+        return this.city;
+    }
+}
